7073eebf6cfd66cd4fcf60ec8b37a6fd8c1a18a1
keawade <keawade@gmail.com>
2015-09-28 19:51:37 -0500

    Basic data structure in progress

---
+++ data.js
@@ -0,0 +1,26 @@
+{
+  rooms: [
+    {
+      description: "You find yourself standing in a small, dimly lit room. There is a small sign with text on it.",
+      commands: {
+        read: {
+          sign: "Welcome to Textlandia! This is a magical world where anything can happen as long as it's in the script.",
+          pamphlet: "Type commands to move around and interact with your environment! You can move in any of the cardinal or intercardinal directions."
+        }
+      },
+      items: {
+        pamphlet: "Type commands to move around and interact with your environment! You can move in any of the cardinal or intercardinal directions."
+      },
+      exits: {
+        n: -1,
+        ne: -1,
+        e: -1,
+        se: -1,
+        s: -1,
+        sw: -1,
+        w: -1,
+        nw: -1
+      }
+    }
+  ]
+}
