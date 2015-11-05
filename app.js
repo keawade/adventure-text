3b8ed81e40ac807c149074cdb1119bc43f5eb498
Keith Wade <keawade@users.noreply.github.com>
2015-11-05 11:28:27 -0600

    Bug fix
    Resolves #1

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ var you = {
   inventory: [],
 
 }
+
+console.log("You find yourself in a small room.");
+console.log("Behind you is a grue!");
+console.log("You were eaten by a grue.");
+console.log("GAME OVER");
